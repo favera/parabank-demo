12f9b36cb65c69b2e95bc7ba5ab0fc8a672a9244
favera <faa88ver@gmail.com>
2021-05-27 21:22:30 -0400

    Create gherkin steps for paybills

---
+++ cypress/integration/payBills.spec.js
@@ -0,0 +1,7 @@
+/****
+ * Scenario: Pay bills and discount from Account
+ * given my account xxxx with available amount greather than $100
+ * when I pay my bills of xxx amount
+ * then my available amount would decrease by xxx amount 
+ *
+*****/
