79dffae5c18140a7ae3be7e03039859ea9d1d7e4
favera <faa88ver@gmail.com>
2021-07-07 19:49:34 -0400

    ****Changes in loginToApplication Command ****
    Change the comand to do the login via request and not from the visual
    form, this acelerate the login process.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,11 +22,24 @@
 //
 //
 // -- This will overwrite an existing command --
+
+import leftPanelMenuPage from "./page-objects/leftPanelMenuPage"
+
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('loginToApplication', ()=> {
-    cy.visit('/')
-    cy.get('input[name="username"]').clear().type(Cypress.env('username'))
-    cy.get('input[name="password"]').clear().type(Cypress.env('password'))
-    cy.get('input.button[value="Log In"]').click()
-    cy.contains('Accounts Overview').should("be.visible")
+    //visual login
+    // cy.visit('/')
+    // cy.get('input[name="username"]').clear().type(Cypress.env('username'))
+    // cy.get('input[name="password"]').clear().type(Cypress.env('password'))
+    // cy.get('input.button[value="Log In"]').click()
+    // cy.contains('Accounts Overview').should("be.visible")
+
+    //login by request, the application don't have token
+    cy.request({
+        method: 'POST',
+        url: 'login.htm',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        form: true,
+        body:{'username': 'john', 'password': 'demo'}
+    })
 })
